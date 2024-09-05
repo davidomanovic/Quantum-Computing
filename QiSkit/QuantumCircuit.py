25f2b9c246b50b4d6bb58b4acf0533d4623f89a4
David Omanovic <117093972+davidomanovic@users.noreply.github.com>
2024-09-05 17:00:17 +0200

    Update QuantumCircuit.py

--- QiSkit/QuantumCircuit.py
+++ QiSkit/QuantumCircuit.py
@@ -1,5 +1,7 @@
 from qiskit import QuantumCircuit
 from qiskit.circuit.library import HGate, MCXGate
+from qiskit.circuit.library import TwoLocal
+
 mcx_gate = MCXGate(3) # Multi-controlled-X gate
 hadamard_gate = HGate()
 
@@ -7,3 +9,6 @@ qc = QuantumCircuit(4)
 qc.append(hadamard_gate, [0])
 qc.append(mcx_gate, [0, 1, 2, 3])
 qc.draw('mpl')
+
+two_local = TwoLocal(3, 'rx', 'cz')
+two_local.decompose().draw('mpl')
